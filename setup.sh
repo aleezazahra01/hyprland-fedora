23012f993d23c1ca6d62d46fc351fe060030c5b7
Aleeza Zahra <127841293+aleezazahra01@users.noreply.github.com>
2026-01-28 23:48:28 +0500

    scripts l

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Hyprland setup script
+# Usage: run from inside your cloned repo: bash setup.sh
+
+mkdir -p ~/.config 
+echo "Creating symlinks..."
+
+ln -sf "$PWD/wallpapers" ~/.config/wallpapers
+
+ln -sf "$PWD/scripts" ~/.config/scripts
+
+ln -sf "$PWD/waybar" ~/.config/waybar
+ln -sf "$PWD/kitty" ~/.config/kitty
+ln -sf "$PWD/wofi" ~/.config/wofi
+
+ln -sf "$PWD/swaylock" ~/.config/swaylock
+ln -sf "$PWD/wlogout" ~/.config/wlogout
+
+ln -sf "$PWD/fastfetch" ~/.config/fastfetch
+
+ln -sf "$PWD/hypr" ~/.config/hypr
+
+ln -sf "$PWD/style" ~/.config/style
+
+echo "All symlinks created"
+echo "You can now start Hyprland"
---
+++ wlogout.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Run wlogout detached so Waybar doesn’t block
+nohup wlogout >/dev/null 2>&1 &
