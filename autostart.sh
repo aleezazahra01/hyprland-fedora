f907f99991ae6d8dd8a9904d972f9f9a7553cf1b
Aleeza Zahra <127841293+aleezazahra01@users.noreply.github.com>
2026-01-31 16:30:21 +0500

    joji

--- autostart.sh
+++ autostart.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
 
-# Export Pywal GTK colors if they exist
 [ -f ~/.cache/wal/colors.sh ] && source ~/.cache/wal/colors.sh
 
-# Use your Hyprland folder as config home
 export XDG_CONFIG_HOME="$HOME/hyprlnd"
 a
-# Set wallpaper & generate Pywal colors
 "$HOME/hyprlnd/scripts/wallcycle.sh"
 "$HOME/hyprlnd/scripts/wofi-way.sh"
 
 
 
-# Start Waybar AFTER CSS exists
 waybar --config "$HOME/hyprlnd/waybar/config" \
     --style "$HOME/hyprlnd/waybar/style.css" &
 
-# Start Kitty
+
 kitty &
 waybar &
-
-# Start swaync
-#swaync --config "$HOME/hyprlnd/swaync/config.json" &
-
-# Start fastfetch
 fastfetch --config "$HOME/hyprlnd/fastfetch/config.jsonc" &
-
-# Start Cava
 "$HOME/hyprlnd/scripts/cava_pywal.sh" &
