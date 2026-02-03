931f55aa11b17fad63cc05aeda65fec1a0718a4f
Aleeza Zahra <127841293+aleezazahra01@users.noreply.github.com>
2026-02-03 22:38:24 +0500

    Update setwall.sh

--- setwall.sh
+++ setwall.sh
@@ -5,6 +5,7 @@ pkill swww 2>/dev/null
 swww-daemon &
 sleep 1
 
+
 WALL_DIR="$HOME/hyprlnd/wallpapers"
 STYLE_TEMPLATE="$HOME/hyprlnd/waybar/style.css.template"
 STYLE_OUTPUT="$HOME/hyprlnd/waybar/style.css"
@@ -15,14 +16,26 @@ CHOICE=$(ls "$WALL_DIR" | wofi --dmenu -p "Select Wallpaper:")
 [ -z "$CHOICE" ] && exit 0
 WALL="$WALL_DIR/$CHOICE"
 
+
 swww img "$WALL" --transition-type outer --transition-duration 1
 
-$HOME/.local/bin/wal -i "$WALL" --backend kitty
+#$HOME/.local/bin/wal -i "$WALL" --backend kitty
+#$HOME/.local/bin/wal -i "$WALL" --backend waybar
+$HOME/.local/bin/wal -i "$WALL"
+pkill waybar
+waybar &
+
+
 sleep 1
 
 kitty @ set-colors --all "$KITTY_COLORS" 2>/dev/null || echo "Kitty remote control not available, colors will apply on new windows"
-# Make sure pywal16 CSS exists
+
+
 if [ ! -f "$PYWAL_CSS" ]; then
+    echo "Pywal16 CSS not found at: $PYWAL_CSS"
+    exit 1
+fi
+
     echo "Pywal16 CSS not found at: $PYWAL_CSS"
     exit 1
 fi
